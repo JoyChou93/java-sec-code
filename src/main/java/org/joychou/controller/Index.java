ce08363eddf38499537212daaeca01a4bcf01362
JoyChou <viarus@qq.com>
2018-05-28 18:08:10 +0800

    add index

---
+++ src/main/java/org/joychou/controller/Index.java
@@ -0,0 +1,23 @@
+package org.joychou.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+/**
+ * author: JoyChou (joychou@joychou.org)
+ * date:   2018.05.28
+ * desc:   Index Page
+ */
+
+@Controller
+public class Index {
+    @RequestMapping("/")
+    @ResponseBody
+    public static String index() {
+        return "Welcome to java sec code home page by JoyChou(joychou@joychou.org)";
+    }
+}
