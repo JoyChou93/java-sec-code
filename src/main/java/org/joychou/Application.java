d1963da5ad5396bf7b1cde36560c98fbe50935bf
JoyChou <viarus@qq.com>
2019-03-04 11:24:24 +0800

    Actuators to RCE

--- src/main/java/org/joychou/Application.java
+++ src/main/java/org/joychou/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application extends SpringBootServletInitializer {
 
     @Override
